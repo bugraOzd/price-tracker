30bb58a130192f3918efe6790b5566bc8e20b9be
bugraOzd <bugraozdilek@gmail.com>
2024-03-14 15:52:16 +0300

    updates

--- components/HeroCarousel.tsx
+++ components/HeroCarousel.tsx
@@ -31,9 +31,9 @@ function HeroCarousel() {
     <div className="hero-carousel">
       <Carousel
         showThumbs={false}
-        autoPlay
+        // autoPlay
         infiniteLoop
-        interval={2000}
+        // interval={2000}
         showArrows={false}
         showStatus={false}
       >
--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,18 +1,65 @@
 "use client";
 
+import { scrapeAndStoreProduct } from "@/lib/actions";
+import { FormEvent, useState } from "react";
+
 const SearchBar = () => {
-  const handleSubmit = () => {};
+  const [searchPrompt, setSearchPrompt] = useState("");
+  const [isLaoding, setisLaoding] = useState(false);
+
+  const isValidAmazonUrl = (url: string) => {
+    try {
+      const parsedUrl = new URL(url);
+      const hostName = parsedUrl.hostname;
+
+      if (
+        hostName.includes("amazon.com") ||
+        hostName.includes("amazon.") ||
+        hostName.endsWith("amazon")
+      ) {
+        return true;
+      }
+    } catch (error) {
+      return false;
+    }
+  };
+
+  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault(); // default behaviour is to reload the page, so we use preventDefault
+
+    console.log(searchPrompt);
+
+    const isValidLink = isValidAmazonUrl(searchPrompt);
+
+    if (!isValidLink) return alert("Please provide a valid Amazon link.");
+
+    try {
+      setisLaoding(true);
+
+      const product = await scrapeAndStoreProduct(searchPrompt);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setisLaoding(false);
+    }
+  };
 
   return (
     <form className="flex flex-wrap gap-4 mt-12" onSubmit={handleSubmit}>
       <input
         type="text"
+        value={searchPrompt}
+        onChange={(e) => setSearchPrompt(e.target.value)}
         placeholder="Enter product link"
         className="searchbar-input"
       ></input>
 
-      <button type="submit" className="searchbar-btn">
-        Search
+      <button
+        type="submit"
+        className="searchbar-btn"
+        disabled={searchPrompt === ""}
+      >
+        {isLaoding ? "Searching..." : "Search"}
       </button>
     </form>
   );
